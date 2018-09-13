e295d537a60922be29b03ad9778ed65801482e2a
parker-zim <pazimmerman@vassar.edu>
2018-09-13 10:58:42 -0400

    Init Commit
    Settup of basic classes

---
+++ name.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+bool gameOver;
+void Setup(){
+  gameOver = false;
+}
+void Draw(){
+
+}
+void Input() {
+
+}
+void Logic(){
+
+}
+
+int main() {
+  Setup()
+  while(!gameOver){
+    Draw();
+    Input();
+    Logic();  
+  }
+  return 0;
+}
